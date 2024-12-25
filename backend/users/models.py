9615159bafed085922360edd3a5f43fa6cbaa746
Jamshid Khaksaar <104823622+jamshidkhaksaar@users.noreply.github.com>
2024-12-25 13:34:30 +0430

    created a fresh django backend project and added api inside the backend folder also removed the task-manager, api directories from the main Task-Management folder

--- backend/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class CustomUser(AbstractUser):
-    """Custom user model extending Django's AbstractUser."""
-    bio = models.TextField(max_length=500, blank=True)
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
