0b0065a8b2ee9be506fd0a3b8f1707c4850dd29e
Jamshid Khaksaar <104823622+jamshidkhaksaar@users.noreply.github.com>
2025-04-26 16:31:03 +0430

    feat: add MailModule and ConfigModule to UsersModule imports
    - Integrated MailModule for email functionalities and ConfigModule for configuration management in UsersModule, enhancing user-related operations and service configurations.

--- backend-nest/src/users/users.module.ts
+++ backend-nest/src/users/users.module.ts
@@ -5,12 +5,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AdminModule } from '../admin/admin.module';
 import { TasksModule } from '../tasks/tasks.module';
+import { MailModule } from '../mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     forwardRef(() => AdminModule), // Use forwardRef to avoid circular dependency
     forwardRef(() => TasksModule), // Add TasksModule import with forwardRef
+    MailModule,
+    ConfigModule,
   ],
   providers: [UsersService],
   controllers: [UsersController],
